4f0f3a591a85004e10ef046f083bedc376fd7908
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2021-09-16 17:59:09 +0300

    complex example

--- packages/enum-converter-web/src/convert/components/ConvertScreen/pythonSample.js
+++ packages/enum-converter-web/src/convert/components/ConvertScreen/pythonSample.js
@@ -1,13 +1,19 @@
-export const PYTHON_SAMPLE_ENUM = `from enum import Enum, auto, IntEnum
+export const PYTHON_SAMPLE_ENUM = `
+from enum import Enum, auto, IntEnum
 
 class Colors(Enum):
     Green = 1
     Yellow = auto()
     Blue = 'Blue'
-
-
+    
 class Pets(Enum):
     Dog = 1
     Cat = 2
     Mouse = 4
+
+
+class ComplexExample(Enum):
+    AndyFirst = 1
+    DaveSecond = 2
+    MouseThird = 4 
 `;
